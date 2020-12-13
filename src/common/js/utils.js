69b539d91c3017107897d999ad1e87445497fbaa
Neil Mistry <neil@amplience.com>
2020-12-13 19:12:04 +0000

    Modanisa Demo (Neil Demo)

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -330,9 +330,45 @@
     poi.init();
   }
 
+  function doCountdowns(){
+    var counters = document.querySelectorAll('[data-amp-counter]');
+    counters.forEach(function (counterdom) {
+      var counddowntime = counterdom.getAttribute('data-amp-counter');
+      var countDownDate = new Date(JSON.parse(counddowntime)).getTime();
+
+      // Update the count down every 1 second
+      var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+          
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+          
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          
+        // Output the result in an element with id="demo"
+        counterdom.innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+          
+        // If the count down is over, write some text 
+        if (distance < 0) {
+          clearInterval(x);
+          counterdom.innerHTML = "EXPIRED";
+        }
+      }, 1000);
+
+    })
+  }
+
   function attachComponents() {
     attachComponent('.amp-dc-promo-banner', PromoBanner);
     attachComponent('.amp-dc-slider', Slider);
+    doCountdowns();
     initPOI();
     setTimeout(scrollCard, 2000);
   }
